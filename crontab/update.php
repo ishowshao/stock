11acb17236d58d5e9d65c2171037f5dccbb376d2
ishowshao <ishowshao@me.com>
2016-04-20 20:11:28 +0800

    crontab script

--- crontab/update.php
+++ crontab/update.php
@@ -6,18 +6,24 @@ define('PATH', dirname(__FILE__));
 require PATH . '/../func/common.func.php';
 require PATH . '/../class/DbClient.php';
 
-$stocks = 'sz300212,sz300059'; // 这个要改成从数据库读取
+$listenStocks = DbClient::getInstance('stock')->getCollection('listen')->find();
+$stocks = array();
+foreach ($listenStocks as $object) {
+    $stocks[] = $object['id'];
+}
 
-$priceData = getSinaData($stocks);
+$priceData = getSinaData(implode(',', $stocks));
 
 var_dump($priceData);
 
-$stockArray = explode(',', $stocks);
-foreach ($stockArray as $i => $stock) {
+foreach ($stocks as $i => $stock) {
     $collection = DbClient::getInstance('stock_history')->getCollection($stock);
     $cursor = $collection->find(array('date' => $priceData[$i]['date']));
     echo 'find ', $priceData[$i]['date'], "\n";
     $found = $cursor->count();
+    if (!$found) {
+        $collection->insert($priceData[$i]);
+    }
     var_dump($found);
 }
 //foreach ($cursor as $data) {
